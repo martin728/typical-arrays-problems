a04dcfe01c42b85ffcb13d01f6c73d3e81f2d118
Diana Martyn <dianamartin790@gmail.com>
2020-10-03 15:25:55 +0300

    feat: functions implemented

--- src/index.js
+++ src/index.js
@@ -1,12 +1,41 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    
+    if (array == '' || array == undefined) {
+        return 0;
+    }
+    let min = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i]
+        }
+    }
+    return min;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (array == '' || array == undefined) {
+        return 0;
+    }
+    let max = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i]
+        }
+    }
+    return max;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+
+
+exports.avg = function avg(array) {
+    let sum = 0;
+    let avg = 0;
+    if (array == '' || array == undefined) {
+        return 0;
+    }
+    for (var i = 0; i < array.length; i++) {
+        sum += array[i];
+        avg = sum / array.length;
+    }
+    return avg;
 }
